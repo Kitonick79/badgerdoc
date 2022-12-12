f36b4c9b02a70412852d34ef37e065f70bef79ab
Andrei Borisevich <andrei_borisevich@epam.com>
2022-12-12 22:37:34 +0300

    add token converter for vertex

--- convert/src/wip_vertex_converter/main.py
+++ convert/src/wip_vertex_converter/main.py
@@ -1,59 +1,82 @@
-import json
 from pathlib import Path
 from typing import List
 
 import fitz
 from fitz import Page
+from pydantic import BaseModel, Field
 
 
-def draw_char(char: str, x0: int, y0: int, page: Page, font_height=11, font_width=7) -> None:
-    x1, y1 = x0 + font_width, y0 + font_height
-    rect = fitz.Rect(x0, y0, x1, y1)
-    shape = page.new_shape()
-    shape.draw_rect(rect)
-    shape.finish(width=0.3, color=(1, 1, 1))
-    shape.insert_textbox(rect, char, fontname='cour')
-    shape.commit()
+class Offset(BaseModel):
+    begin: int
+    end: int
 
 
-def draw_string(string: str, x0: int, y0: int, page: Page, font_height=11, font_width=7) -> None:
-    x, y = x0, y0
-    for char in string:
-        draw_char(char, x, y, page)
-        x += font_width
+class BadgerdocToken(BaseModel):
+    type_: str = Field(default="text", alias="type")
+    bbox: List[float]
+    text: str
 
 
 def generate_chunks(text: str, n=70) -> List[str]:
     return [text[i:i + n] for i in range(0, len(text), n)]
 
 
-def draw_pharagraph(text: str, x0: int, y0: int, page: Page, font_height=11, font_width=7, n=80) -> None:
-    chunks = generate_chunks(text, n=n)
-    x, y = x0, y0
-    for chunk in chunks:
-        draw_string(chunk, x, y, page)
-        y += font_height * 2
+class PlainTextToBadgerdocTokenConverter:
+    PAGE_WIDTH = 595
+    PAGE_BORDER_OFFSET = 15
+    FONT_HEIGHT = 11
+    FONT_WIDTH = 7
 
+    def draw_tokens(self, tokens: List[BadgerdocToken], save_path):
+        doc = fitz.open()
+        width = max((token.bbox[2] for token in tokens)) + self.PAGE_BORDER_OFFSET
+        height = max((token.bbox[3] for token in tokens)) + self.PAGE_BORDER_OFFSET
+        page = doc.new_page(height=height, width=width)
+        for token in tokens:
+            self.draw_token(token, page)
+        doc.save(save_path)
 
-def draw_text(text: str, x0: int, y0: int, page: Page, font_height=11, font_width=7, n=80) -> None:
-    text_lines = text.split("\n")
-    x, y = x0, y0
-    for text_line in text_lines:
-        chunks = generate_chunks(text_line, n=n)
-        pharagraph_height = len(chunks) * font_height * 2
-        draw_pharagraph(text_line + "\n", x, y, page, font_height, font_width, n)
-        y += pharagraph_height
+    def draw_token(self, token: BadgerdocToken, page: Page) -> None:
+        rect = fitz.Rect(token.bbox[0], token.bbox[1], token.bbox[2], token.bbox[3])
+        shape = page.new_shape()
+        shape.draw_rect(rect)
+        shape.finish(width=0.3, color=(0.8, 0.8, 1))
+        shape.insert_textbox(rect, token.text, fontname='cour')
+        shape.commit()
+
+    @property
+    def line_length_token(self):
+        return int((self.PAGE_WIDTH - 2 * self.PAGE_BORDER_OFFSET) / self.FONT_WIDTH)
+
+    def convert(self, text: str) -> List[BadgerdocToken]:
+        tokens = []
+        paragraphs = (paragraph + "\n" for paragraph in text.split("\n"))
+
+        y = self.PAGE_BORDER_OFFSET
+        for paragraph in paragraphs:
+            lines = generate_chunks(paragraph, self.line_length_token)
+            for line in lines:
+                self.convert_line(line, tokens, y)
+                y += self.FONT_HEIGHT * 2
+            y += self.FONT_HEIGHT
+
+        return tokens
+
+    def convert_line(self, line, tokens, y):
+        x = self.PAGE_BORDER_OFFSET
+        for char in line:
+            token = BadgerdocToken(bbox=[x, y, x + self.FONT_WIDTH, y + self.FONT_HEIGHT], text=char)
+            tokens.append(token)
+            x += self.FONT_WIDTH
 
 
 def main():
-    doc = fitz.open()
-    page = doc.new_page(height=2000)
-
     text = Path("tests/test.txt").read_text() * 3
-    draw_text(text, 20, 60, page)
 
-    doc.save("allmyimages.pdf")
-    print(len(text))
+    converter = PlainTextToBadgerdocTokenConverter()
+    tokens = converter.convert(text)
+    print(tokens)
+    converter.draw_tokens(tokens, "result.pdf")
 
 
 if __name__ == "__main__":
