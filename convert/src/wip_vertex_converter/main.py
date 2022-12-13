5d8e5b75006b9223c9cea8d96ba2b0d32fdeae9c
Sergei Smolov <sergei_smolov@epam.com>
2022-12-13 15:42:18 +0400

    Add offset to BD format

--- convert/src/wip_vertex_converter/main.py
+++ convert/src/wip_vertex_converter/main.py
@@ -1,4 +1,6 @@
+import json
 from pathlib import Path
+from pprint import pprint
 from typing import List
 
 import fitz
@@ -15,6 +17,7 @@ class BadgerdocToken(BaseModel):
     type_: str = Field(default="text", alias="type")
     bbox: List[float]
     text: str
+    offset: Offset
 
 
 def generate_chunks(text: str, n=70) -> List[str]:
@@ -50,24 +53,38 @@ class PlainTextToBadgerdocTokenConverter:
 
     def convert(self, text: str) -> List[BadgerdocToken]:
         tokens = []
+        line_offset = 0
         paragraphs = (paragraph + "\n" for paragraph in text.split("\n"))
 
         y = self.PAGE_BORDER_OFFSET
         for paragraph in paragraphs:
             lines = generate_chunks(paragraph, self.line_length_token)
             for line in lines:
-                self.convert_line(line, tokens, y)
+                self.convert_line(line, tokens, y, line_offset=line_offset)
                 y += self.FONT_HEIGHT * 2
+                line_offset += len(line)
             y += self.FONT_HEIGHT
 
         return tokens
 
-    def convert_line(self, line, tokens, y):
+    def convert_line(self, line, tokens, y, line_offset: int):
         x = self.PAGE_BORDER_OFFSET
-        for char in line:
-            token = BadgerdocToken(bbox=[x, y, x + self.FONT_WIDTH, y + self.FONT_HEIGHT], text=char)
+        for i, char in enumerate(line):
+            begin = line_offset + i
+            end = begin + 1
+            offset = Offset(begin=begin, end=end)
+            token = BadgerdocToken(bbox=[x, y, x + self.FONT_WIDTH, y + self.FONT_HEIGHT], text=char, offset=offset)
             tokens.append(token)
             x += self.FONT_WIDTH
+
+
+class VertexToBadgerdocAnnotationConverter:
+    def __init__(self, vertex_file: Path):
+        self._vertext_file = vertex_file
+        self.vertex_annotation = json.loads(self._vertext_file.read_text())
+
+    def convert(self, badgerdoc_tokens: List[BadgerdocToken]):
+        pass
 
 
 def main():
@@ -75,7 +92,7 @@ def main():
 
     converter = PlainTextToBadgerdocTokenConverter()
     tokens = converter.convert(text)
-    print(tokens)
+    pprint(tokens)
     converter.draw_tokens(tokens, "result.pdf")
 
 
