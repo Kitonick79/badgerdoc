8231d3c3e68c1283534ea1bd1dc1b24dbe0a4d6e
Andrei Borisevich <andrei_borisevich@epam.com>
2022-12-13 16:17:34 +0300

    add annotation converter

--- convert/src/wip_vertex_converter/main.py
+++ convert/src/wip_vertex_converter/main.py
@@ -1,7 +1,7 @@
 import json
 from pathlib import Path
 from pprint import pprint
-from typing import List
+from typing import List, Dict, Any, Tuple
 
 import fitz
 from fitz import Page
@@ -20,13 +20,35 @@ class BadgerdocToken(BaseModel):
     offset: Offset
 
 
+class VertexAnnotationToken(BaseModel):
+    id_: str = Field(alias="id")
+    begin: int
+    end: int
+    entity_type: str
+    entity_name: str
+    links: List[str]
+
+
+class BadgerdocAnnotationToken(BaseModel):
+    id_: int = Field(alias="id")
+    type_: str = Field(default="text", alias="type")
+    bbox: List[float]
+    tokens: List[int]
+    category: str
+    links: List[int]
+    data: Dict[str, Any]
+
+
 def generate_chunks(text: str, n=70) -> List[str]:
     return [text[i:i + n] for i in range(0, len(text), n)]
 
 
+PAGE_BORDER_OFFSET = 15
+
+
 class PlainTextToBadgerdocTokenConverter:
     PAGE_WIDTH = 595
-    PAGE_BORDER_OFFSET = 15
+    PAGE_BORDER_OFFSET = PAGE_BORDER_OFFSET
     FONT_HEIGHT = 11
     FONT_WIDTH = 7
 
@@ -54,7 +76,8 @@ class PlainTextToBadgerdocTokenConverter:
     def convert(self, text: str) -> List[BadgerdocToken]:
         tokens = []
         line_offset = 0
-        paragraphs = (paragraph + "\n" for paragraph in text.split("\n"))
+        paragraphs = [paragraph + "\n" for paragraph in text.split("\n")]
+        paragraphs[-1] = paragraphs[-1][:-1]
 
         y = self.PAGE_BORDER_OFFSET
         for paragraph in paragraphs:
@@ -79,21 +102,73 @@ class PlainTextToBadgerdocTokenConverter:
 
 
 class VertexToBadgerdocAnnotationConverter:
-    def __init__(self, vertex_file: Path):
-        self._vertext_file = vertex_file
-        self.vertex_annotation = json.loads(self._vertext_file.read_text())
 
-    def convert(self, badgerdoc_tokens: List[BadgerdocToken]):
-        pass
+    def convert(self, badgerdoc_tokens: List[BadgerdocToken], vertext_annotation_raw: List[Dict]):
+        vertext_annotation = [VertexAnnotationToken.parse_obj(i) for i in vertext_annotation_raw]
+        bd_annotation = []
+        for vertex_token in vertext_annotation:
+            tokens = badgerdoc_tokens[vertex_token.begin: vertex_token.end]
+            bbox = [
+                min([t.bbox[0] for t in tokens]),
+                min([t.bbox[1] for t in tokens]),
+                max([t.bbox[2] for t in tokens]),
+                max([t.bbox[3] for t in tokens]),
+            ]
+            bd_token = BadgerdocAnnotationToken(
+                id=int(vertex_token.id_),
+                links=[int(i) for i in vertex_token.links],
+                category=vertex_token.entity_type,
+                data={"entity": {"id": vertex_token.entity_name}},
+                tokens=list(range(vertex_token.begin, vertex_token.end)),
+                bbox=bbox
+            )
+            bd_annotation.append(bd_token)
+
+        page_width = max([t.bbox[2] for t in badgerdoc_tokens]) + PAGE_BORDER_OFFSET
+        page_height = max([t.bbox[2] for t in badgerdoc_tokens]) + PAGE_BORDER_OFFSET
+        return {
+            "revision": "revision-1",
+            "pages": [
+                {
+                    "size": {
+                        "width": page_width,
+                        "height": page_height
+                    },
+                    "page_num": 1,
+                    "objs": [i.dict(by_alias=True) for i in bd_annotation],
+                    "validated": [],
+                    "failed_validation_pages": []
+                }
+            ]
+        }
+
+
+def read_vertex_annotation(path_to_file) -> Dict:
+    return json.loads(Path(path_to_file).read_text())
+
+
+def gather_badgerdoc_token_dict(tokens):
+    return {
+        "size": {
+            "width": 123,
+            "height": 123
+        },
+        "page_num": 1,
+        "objs": [i.dict(by_alias=True) for i in tokens]
+    }
 
 
 def main():
-    text = Path("tests/test.txt").read_text() * 3
-
     converter = PlainTextToBadgerdocTokenConverter()
-    tokens = converter.convert(text)
-    pprint(tokens)
+
+    vertex = read_vertex_annotation("tests/input_from_vertex.json")
+
+    tokens = converter.convert(vertex["text"])
     converter.draw_tokens(tokens, "result.pdf")
+
+    annot_converter = VertexToBadgerdocAnnotationConverter().convert(tokens, vertex["annotations"])
+
+
 
 
 if __name__ == "__main__":
--- convert/src/wip_vertex_converter/tests/test_import.py
+++ convert/src/wip_vertex_converter/tests/test_import.py
@@ -8,155 +8,29 @@
 import json
 from pathlib import Path
 
-#
-# INPUT_EXAMPLE_FILE_PATH = "./input_example.json"
-#
-#
-# input_data = json.loads(Path(INPUT_EXAMPLE_FILE_PATH).read_text())
-#
-# output_badgerdoc_tokens = {
-#     # In additional we will generate PDF file for these tokens.
-#     "size": {"width": 123, "height": 123},
-#     "page_num": 1,
-#     "objs": [
-#         {
-#             "type": "text",
-#             "bbox": [74.986, 82.455, 201.588, 99.671],
-#             "text": "w",
-#             "offset": {"begin": 0, "end": 1}
-#         },
-#         {
-#             "type": "text",
-#             "bbox": [205.892, 82.455, 301.248, 99.671],
-#             "text": "1",
-#             "offset": {"begin": 1, "end": 2}
-#         },
-#         {
-#             "type": "text",
-#             "bbox": [205.892, 82.455, 301.248, 99.671],
-#             "text": " ",
-#             "offset": {"begin": 2, "end": 3}
-#         },
-#         {
-#             "type": "text",
-#             "bbox": [305.552, 82.455, 328.844, 99.671],
-#             "text": "2",
-#             "offset": {"begin": 3, "end": 4}
-#         },
-#         {
-#             "type": "text",
-#             "bbox": [305.552, 82.455, 328.844, 99.671],
-#             "text": "\n",
-#             "offset": {"begin": 4, "end": 5}
-#         },
-#         {
-#             "type": "text",
-#             "bbox": [305.552, 82.455, 328.844, 99.671],
-#             "text": "3",
-#             "offset": {"begin": 5, "end": 6}
-#         },
-#         {
-#             "type": "text",
-#             "bbox": [305.552, 82.455, 328.844, 99.671],
-#             "text": "\t",
-#             "offset": {"begin": 6, "end": 7}
-#         },
-#         {
-#             "type": "text",
-#             "bbox": [305.552, 82.455, 328.844, 99.671],
-#             "text": "4",
-#             "offset": {"begin": 7, "end": 8}
-#         },
-#     ]
-# }
-#
-# output_badgerdoc_annotation = {
-#     "revision": "revision-1",  # TODO: What is it?
-#     "pages": [  # As far as we understand we will fill only this field
-#         {
-#             "size": {
-#                 "width": 0,
-#                 "height": 0
-#             },
-#             "page_num": 1,
-#             "objs": [
-#                 {
-#                     "id": 1,
-#                     "type": "box",  # Which type should we use?
-#                     "bbox": [75, 82, 537, 119],
-#                     "category": "some, VAT, 19",  # We suppose we should put here entity type and entity name?
-#                     "entity_type": "some",
-#                     "entity_name": "VAT, 19"
-#                 },
-#                 {
-#                     "id": 42,
-#                     "type": "box",
-#                     "bbox": [53, 266, 295, 487],
-#                     "category": "IMPOSITION, VAT, 19",
-#                     "entity_type": "IMPOSITION",
-#                     "entity_name": "VAT, 19"
-#                 },
-#                 {
-#                     "id": 43,
-#                     "type": "box",
-#                     "bbox": [317, 406, 558, 447],
-#                     "category": "232323, VAT, 19",
-#                     "entity_type": "232323",
-#                     "entity_name": "VAT, 19",
-#                     "links": [42, 1]
-#                 }
-#             ]
-#         }
-#     ],
-#     "validated": [],
-#     "failed_validation_pages": []
-# }
-#
-# badgerdoc_data = {
-#     "pdf_file": "path/to/file",
-#     "tokens": output_badgerdoc_tokens,
-#     "annotation": output_badgerdoc_annotation
-# }
+from ..main import PlainTextToBadgerdocTokenConverter, read_vertex_annotation, VertexToBadgerdocAnnotationConverter, \
+    gather_badgerdoc_token_dict
+
+INPUT_VERTEX_FILE = "./tests/input_from_vertex.json"
+BADGERDOC_TOKENS_FILE = "./tests/badgerdoc_tokens.json"
+BADGERDOC_ANNOTATIONS_FILE = "./tests/badgerdoc_annotation.json"
 
 
-# def test_convert_tokens():
-#     input_text = "q we"  # TODO: add `\n`
-#     calculated_width = 612.0
-#     calculated_height = 792.0
-#
-#     expected_tokens = [
-#         {
-#             "size": {"width": calculated_width, "height": calculated_height},
-#             "page_num": 1,
-#             "objs": [
-#                 {
-#                     "type": "text",
-#                     "bbox": [74.986, 82.455, 201.588, 99.671],
-#                     "text": "q",
-#                     "offset": {"begin": 0, "end": 1}
-#                 },
-#                 {
-#                     "type": "text",
-#                     "bbox": [205.892, 82.455, 301.248, 99.671],
-#                     "text": " ",
-#                     "offset": {"begin": 1, "end": 2}
-#                 },
-#                 {
-#                     "type": "text",
-#                     "bbox": [205.892, 82.455, 301.248, 99.671],
-#                     "text": "w",
-#                     "offset": {"begin": 2, "end": 3}
-#                 },
-#                 {
-#                     "type": "text",
-#                     "bbox": [305.552, 82.455, 328.844, 99.671],
-#                     "text": "e",
-#                     "offset": {"begin": 3, "end": 4}
-#                 },
-#             ]
-#         }
-#     ]
-#
-#
-def test_convert_annotation():
-    ...
+def general_test():
+    vertex_data = read_vertex_annotation(INPUT_VERTEX_FILE)
+
+    converter = PlainTextToBadgerdocTokenConverter()
+    tokens = converter.convert(vertex_data["text"])
+    converter.draw_tokens(tokens, "result.pdf")
+    bd_annot = VertexToBadgerdocAnnotationConverter().convert(tokens, vertex_data["annotations"])
+    tokens_dict = gather_badgerdoc_token_dict(tokens)
+
+    expected_bd_tokens = json.loads(Path(BADGERDOC_TOKENS_FILE).read_text())
+    expected_bd_annotation = json.loads(Path(BADGERDOC_ANNOTATIONS_FILE).read_text())
+
+    json.dump(bd_annot, open("filename.json", 'w'), indent=4)
+    assert tokens_dict == expected_bd_tokens
+    assert bd_annot == expected_bd_annotation
+
+
+general_test()
